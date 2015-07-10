a977dd1f2b2b1e0e77b01531101fa2b460908793
Simon Watson <v2wm8ryazf@snkmail.com>
2015-07-10 23:57:24 +0100

    CSV loading and parsing seems to be working now.  Had a fun little issue which turned out to be a problem with the text encoding on the test data being different from the live data. Next step is probably building the UI and hooking in the data.

--- DevTest-Lib/Source/CSVParser.cs
+++ DevTest-Lib/Source/CSVParser.cs
@@ -1,21 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace DevTestLib
 {
 	public class CSVParser : IParser
 	{
-		public int Count { get { return 0; } }
+		public int Count 
+		{ 
+			get 
+			{ 
+				if (parsedCSV == null)
+					return 0;
+				
+				return parsedCSV.Length; 
+			} 
+		}
 
 		public string[] this[int i]
 		{
 			get 
 			{
-				return new string[] { };
+				if (i >= Count)
+					return new string[0];
+				
+				return parsedCSV[i];
 			}
 		}
 
-		public CSVParser (string assetName)
+		private string[][] parsedCSV = null;
+
+		private readonly char[] trimChars = { '"' };
+		private readonly string[] splitStrings = { "\",\"" };
+
+		public CSVParser (Stream input)
 		{
+			if (input == null)
+				return;
+
+			var reader = new StreamReader (input, System.Text.Encoding.UTF8, true, 2048);
+
+			if (reader.EndOfStream)
+				return;
+
+			reader.ReadLine (); // Ditch the titles
+
+			if (reader.EndOfStream)
+				return;
+			
+			var stringList = new List<string[]> ();
+
+			string line;
+
+
+			while (!reader.EndOfStream)
+			{
+				line = reader.ReadLine();							
+				var items = line.Split(splitStrings, StringSplitOptions.None);		// split on "," to avoid splitting in mid text
+																					// keep empty entries
+
+				items[0] = items[0].Trim (trimChars); // Take the quotes off the ends of the first and last strings
+				items[items.Length-1] = items[items.Length-1].Trim (trimChars);
+
+				stringList.Add (items);
+			}
+
+			//TODO: We should probably check if the lines are all the same length here
+			parsedCSV = stringList.ToArray();
 		}
 	}
 }
--- DevTest-UnitTest/Source/CSVParser_Tests.cs
+++ DevTest-UnitTest/Source/CSVParser_Tests.cs
@@ -1,46 +1,63 @@
 ﻿using System;
 using NUnit.Framework;
 using DevTestLib;
-
+using Android.Content.Res;
+using System.IO;
 
 namespace DevTestUnitTest
 {
 	[TestFixture]
 	public class CSVParser_Tests
 	{
+		AssetManager assets;
+		Stream input;
+
 		[SetUp]
-		public void Setup ()
+		public void Setup()
 		{
+			assets = Android.App.Application.Context.Assets;
+			input = assets.Open ("Data/titleTest2.csv");
 		}
 
 		[TearDown]
-		public void Tear ()
+		public void Tear()
 		{
+			input.Close();
 		}
 
 		[Test]
 		public void LoadingEmptyFile ()
 		{
-			var parser = new CSVParser("");
+			var parser = new CSVParser(null);
 
-			Assert.True (parser.Count == 0);
+			Assert.That(parser.Count, Is.EqualTo(0));		
 		}
 
 		[Test]
 		public void LoadingTestFile ()
 		{
-			var parser = new CSVParser("Data/titleTest2.csv");
+			var parser = new CSVParser(input);
 
-			Assert.True (parser.Count == 11);
+			Assert.That(parser.Count, Is.EqualTo(11));
 		}
 
 		[Test]
 		public void FirstLineCorrect ()
 		{
-			var parser = new CSVParser("Data/titleTest2.csv");
+			var parser = new CSVParser(input);
 
 			var testArray = new[] { "1001 spot illustrations of the lively twenties","La Vie Parisienne","008077927","GB8865678","0486250210","","","","","Grafton, Carol Belanger","","","England","New York ; London","Dover ; Constable","1986","123 pages, chiefly illustrations, 28 cm","741.5944","YV.1988.b.2439","Design--History--20th century--Themes, motives ; Art deco ; French caricatures--1920-1930","","Illustrations from La Vie Parisienne"};
-			Assert.True (parser[0] == testArray);
+			Assert.That(parser[0], Is.EqualTo(testArray));
+		}	
+
+		// Noticed this while debugging, there's an issue when the last element is empty
+		[Test]
+		public void FourthLineCorrect ()
+		{
+			var parser = new CSVParser(input);
+
+			var testArray = new[] { "How Obelix fell into the magic potion","","015103770","GBA933631","9781444000269 ; 1444000268","Goscinny","1926-1977","person","","Goscinny ; Uderzo","","","England","London","Orion Children's","2009","1 v, chiefly illustrations, 29 cm","741.5","","Astérix (Fictitious character)--Comic books, strips, etc--Juvenile fiction ; Obelix (Fictitious character : Uderzo)--Comic books, strips, etc--Juvenile fiction","",""};
+			Assert.That(parser[3], Is.EqualTo(testArray));
 		}	
 	}
 }
